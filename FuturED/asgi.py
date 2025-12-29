280faa08d2df57b351cec5d19f15ea3bf6afae72
Aadarsha Bhusal <157955330+aadarshabhusal@users.noreply.github.com>
2025-12-29 22:52:13 +0545

    modify

--- FuturED/asgi.py
+++ FuturED/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FuturED.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'futured.settings')
 
 application = get_asgi_application()
